4a3c4af1afb04995b4feca27ab7dcde85dd28105
Bhuvana57 <130349892+Bhuvana57@users.noreply.github.com>
2025-11-13 07:42:33 +0530

    Fix indentation error and initialize cache in views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -88,13 +88,13 @@ def authorized():
     if request.args.get('code'):
         cache = _load_cache()
         # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
-        if "error" in result:
-            return render_template("auth_error.html", result=result)
-        session["user"] = result.get("id_token_claims")
                 result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
             request.args['code'],
             scopes=Config.SCOPE,
             redirect_uri=url_for('authorized', _external=True, _scheme='https')
+                                if "error" in result:
+                                    return render_template("auth_error.html", result=result)
+                    session["user"] = result.get("id_token_claims")
         )
         # Note: In a real app, we'd use the 'name' property from session["user"] below
         # Here, we'll use the admin username for anyone who is authenticated by MS
@@ -118,6 +118,7 @@ def logout():
 
 def _load_cache():
     # TODO: Load the cache from `msal`, if it exists
+        cache = msal.SerializableTokenCache()
 
         if session.get('token_cache'):
         cache.deserialize(session['token_cache'])
